9c55677b6698efd28d58d0e2bf86853a2bed09b6
sebastien-ehouan <sibous05@gmail.com>
2020-07-21 09:12:19 -0400

    Refreshing the repo

--- src/tcore/RulePrimitive 2.java
+++
@@ -1,8 +0,0 @@
-package tcore;
-
-import tcore.messages.Packet;
-
-public abstract class RulePrimitive extends Primitive {
-	public abstract Packet packetIn(Packet p);
-	
-}
--- src/tcore/messages/exceptions/EmptyControlPrimitiveException 2.java
+++
@@ -1,9 +0,0 @@
-package tcore.messages.exceptions;
-
-/**
- * @author Pierre-Olivier Talbot
- * @version 0.5
- * @since 2017-12-09
- */
-public class EmptyControlPrimitiveException extends TCoreException {
-}

b00c4f68f393fa819482c934362a4f9b6732378d
An Li <31196165+an-li@users.noreply.github.com>
2021-12-05 10:47:01 -0500

    Add more tests for single match and no match

--- src/unit/tests/MatcherTest.java
+++ src/unit/tests/MatcherTest.java
@@ -120,4 +120,51 @@ class MatcherTest {
 
 		assertTrue(ms.equals(p.getCurrentMatchSet()), "Wrong match found");
 	}
+	
+	@Test
+	public void singleMatch2VF2() throws Exception {
+		utils.Utils.initialize();
+
+		// Imports
+		MetaModel OracleMM = new MetaModel("Oracle", "../Ramifier_New/Model/Oracle.ecore"); // Oracle MetaModel
+		MetaModel Oracle_ramified = new MetaModel("OracleRoot", "../Ramifier_New/Model/Oracle_augmented.ecore"); // Ramified Oracle
+
+		Model oracle = new Model("Oracle", "../Ramifier_New/Model/Oracle.xmi", OracleMM); // Dynamic Instance from Oracle
+
+		Pattern SingleMatch_pre = new Pattern("SingleMatch_pre", "../Ramifier_New/Model/SingleMatch_pre2.xmi",
+				Oracle_ramified);
+
+		Packet p = new Packet(oracle);
+		LHS lhs = new LHS(SingleMatch_pre, null);
+
+		// Testing
+		Matcher tester = new Matcher(lhs, 5, true); // max=1
+
+		@SuppressWarnings("unused")
+		Packet result = tester.packetIn(p);
+
+		Match expectedMatch = new Match();
+
+		for (EObject o : oracle.getObjects()) {
+			switch (EcoreUtil.getID(o)) {
+			case "3":
+				expectedMatch.addMapping("3", o); // Single Match case
+				break;
+
+			default:
+				break;
+			}
+		}
+
+		// Array of matches expected to be found
+		ArrayList<Match> expectedMatchArray = new ArrayList<Match>();
+		expectedMatchArray.add(expectedMatch);
+
+		// Expected MatchSet to find
+		MatchSet ms = new MatchSet(expectedMatchArray, lhs);
+
+		assertTrue(tester.isSuccess(), "Matcher failed");
+
+		assertTrue(ms.equals(p.getCurrentMatchSet()), "Wrong match found");
+	}
 }
--- src/unit/tests/MatcherZeroMatchTest.java
+++ src/unit/tests/MatcherZeroMatchTest.java
@@ -93,4 +93,72 @@ class MatcherZeroMatchTest {
         assertTrue(tester.isSuccess(),"Matcher failed");        
         assertTrue(ms.equals(p.getCurrentMatchSet()),"Wrong match found");
 	}
+	
+	@Test
+	public void isSuccessWrongClassVF2() throws Exception {
+		utils.Utils.initialize();
+
+        // Imports
+		MetaModel OracleMM = new MetaModel("Oracle", "../Ramifier_New/Model/Oracle.ecore"); //Oracle MetaModel
+        MetaModel Oracle_augmented = new MetaModel("OracleRoot", "../Ramifier_New/Model/Oracle_augmented.ecore"); //Ramified Oracle
+        
+        Model oracle = new Model("Oracle", "../Ramifier_New/Model/Oracle.xmi", OracleMM); //Dynamic Instance from Oracle
+        
+        Pattern ZeroMatch_pre = new Pattern("ZeroMatch_pre", "../Ramifier_New/Model/ZeroMatch_pre_wrongclass.xmi", Oracle_augmented); //precondition
+
+        Packet p = new Packet(oracle);
+        LHS lhs = new LHS(ZeroMatch_pre, null);
+        
+        //Testing
+        Matcher tester = new Matcher(lhs, 5, true);  //max=1
+        
+		@SuppressWarnings("unused")
+		Packet result = tester.packetIn(p);
+		
+		@SuppressWarnings("unused")
+		Match expectedMatch = new Match();
+		
+		//Array of matches expected to be found
+		ArrayList<Match> expectedMatchArray = new ArrayList<Match>();
+		
+		//Expected MatchSet to find
+        MatchSet ms = new MatchSet(expectedMatchArray,lhs);
+  		
+        assertTrue(tester.isSuccess(),"Matcher failed");        
+        assertTrue(ms.equals(p.getCurrentMatchSet()),"Wrong match found");
+	}
+	
+	@Test
+	public void isSuccessLabelDoesNotExistVF2() throws Exception {
+		utils.Utils.initialize();
+
+        // Imports
+		MetaModel OracleMM = new MetaModel("Oracle", "../Ramifier_New/Model/Oracle.ecore"); //Oracle MetaModel
+        MetaModel Oracle_augmented = new MetaModel("OracleRoot", "../Ramifier_New/Model/Oracle_augmented.ecore"); //Ramified Oracle
+        
+        Model oracle = new Model("Oracle", "../Ramifier_New/Model/Oracle.xmi", OracleMM); //Dynamic Instance from Oracle
+        
+        Pattern ZeroMatch_pre = new Pattern("ZeroMatch_pre", "../Ramifier_New/Model/ZeroMatch_pre_labeldoesnotexist.xmi", Oracle_augmented); //precondition
+
+        Packet p = new Packet(oracle);
+        LHS lhs = new LHS(ZeroMatch_pre, null);
+        
+        //Testing
+        Matcher tester = new Matcher(lhs, 5, true);  //max=1
+        
+		@SuppressWarnings("unused")
+		Packet result = tester.packetIn(p);
+		
+		@SuppressWarnings("unused")
+		Match expectedMatch = new Match();
+		
+		//Array of matches expected to be found
+		ArrayList<Match> expectedMatchArray = new ArrayList<Match>();
+		
+		//Expected MatchSet to find
+        MatchSet ms = new MatchSet(expectedMatchArray,lhs);
+  		
+        assertTrue(tester.isSuccess(),"Matcher failed");        
+        assertTrue(ms.equals(p.getCurrentMatchSet()),"Wrong match found");
+	}
 }

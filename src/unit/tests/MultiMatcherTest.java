394482f95621fcdbb5ec32b74a1a73cbacf23aef
Maxime Boissonnat <boissonnatmaxime@gmail.com>
2022-12-03 23:07:17 -0500

    OK DEMO avec deux tests dans multi matcher

--- src/unit/tests/MultiMatcherTest.java
+++ src/unit/tests/MultiMatcherTest.java
@@ -31,42 +31,48 @@ class MultiMatcherTest {
 	public void setUp() {
 	}
 
-	/*
-	 * @Test public void isSuccess() throws Exception { utils.Utils.initialize();
-	 * 
-	 * // Imports MetaModel OracleMM = new MetaModel("Oracle",
-	 * "../Ramifier_New/Model/Oracle.ecore"); // Oracle MetaModel MetaModel
-	 * Oracle_ramified = new MetaModel("OracleRoot",
-	 * "../Ramifier_New/Model/Oracle_augmented.ecore"); // Ramified Oracle
-	 * 
-	 * Model oracle = new Model("Oracle", "../Ramifier_New/Model/Oracle.xmi",
-	 * OracleMM); // Dynamic Instance from Oracle
-	 * 
-	 * Pattern pre_A = new Pattern("pre_A",
-	 * "../Ramifier_New/Model/MultiMatcher_pre.xmi", Oracle_ramified);
-	 * 
-	 * Packet p = new Packet(oracle); LHS lhs = new LHS(pre_A, null);
-	 * 
-	 * // Testing Matcher tester = new Matcher(lhs, 5,
-	 * JTCoreConstant.SIMPLEMATCH_ALGORITHM);
-	 * 
-	 * @SuppressWarnings("unused") Packet result = tester.packetIn(p);
-	 * 
-	 * Match expectedMatch = new Match();
-	 * 
-	 * for (EObject o : oracle.getObjects()) { switch (EcoreUtil.getID(o)) { case
-	 * "1": expectedMatch.addMapping("1", o); break; case "3":
-	 * expectedMatch.addMapping("3", o); break; default: break; } }
-	 * 
-	 * // Array of matches expected to be found ArrayList<Match> expectedMatchArray
-	 * = new ArrayList<Match>(); expectedMatchArray.add(expectedMatch);
-	 * 
-	 * // Expected MatchSet to find MatchSet ms = new MatchSet(expectedMatchArray,
-	 * lhs);
-	 * 
-	 * assertTrue(tester.isSuccess(), "Matcher failed");
-	 * assertTrue(ms.equals(p.getCurrentMatchSet()), "Wrong match found"); }
-	 */
+	
+	  @Test public void isSuccess() throws Exception { utils.Utils.initialize();
+	  
+	  // Imports 
+	  MetaModel OracleMM = new MetaModel("Oracle", "../Ramifier_New/Model/Oracle.ecore"); // Oracle MetaModel 
+	  MetaModel Oracle_ramified = new MetaModel("OracleRoot", "../Ramifier_New/Model/Oracle_augmented.ecore"); // Ramified Oracle
+	  
+	  Model oracle = new Model("Oracle", "../Ramifier_New/Model/Oracle.xmi", OracleMM); // Dynamic Instance from Oracle
+	  
+	  Pattern pre_A = new Pattern("pre_A", "../Ramifier_New/Model/MultiMatcher_pre.xmi", Oracle_ramified);
+	  
+	  Packet p = new Packet(oracle); LHS lhs = new LHS(pre_A, null);
+	  
+	  // Testing 
+	  Matcher tester = new Matcher(lhs, 5, JTCoreConstant.SIMPLEMATCH_ALGORITHM);
+	  
+	  @SuppressWarnings("unused") Packet result = tester.packetIn(p);
+	  
+	  Match expectedMatch = new Match();
+	  
+	  for (EObject o : oracle.getObjects()) { 
+		  switch (EcoreUtil.getID(o)) { 
+		  case "1": 
+			  expectedMatch.addMapping("1", o); 
+			  break; 
+		  case "3":
+			  expectedMatch.addMapping("3", o); 
+			  break; 
+		  default: 
+			  break; 
+		  } 
+	  }
+	  
+	  // Array of matches expected to be found 
+	  ArrayList<Match> expectedMatchArray = new ArrayList<Match>(); expectedMatchArray.add(expectedMatch);
+	  
+	  // Expected MatchSet to find 
+	  MatchSet ms = new MatchSet(expectedMatchArray, lhs);
+	  
+	  assertTrue(tester.isSuccess(), "Matcher failed");
+	  assertTrue(ms.equals(p.getCurrentMatchSet()), "Wrong match found"); }
+	 
 
 	@Test
 	public void isSuccessVF2() throws Exception {
